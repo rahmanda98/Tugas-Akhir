c75f5a6a69e9fe27821f364fadab06a8a76498f2
Giorno Giovanna <naufala47@gmail.com>
2021-03-27 15:56:12 +0700

    added drawer

--- App.js
+++ App.js
@@ -2,21 +2,8 @@ import React from 'react'
 import { Provider } from 'react-native-paper'
 import { theme } from './src/core/theme'
 import {
-<<<<<<< HEAD
   DrawerMenu,
   StackMenu
-=======
-  StartScreen,
-  LoginScreen,
-  RegisterScreen,
-  ForgotPasswordScreen,
-  Dashboard,
-  DetailMenu,
-  Cart,
-  Checkout,
-  Payment,
-  Profile
->>>>>>> 26acde358bb91f0d7664b41e0486a28b27060cdb
 } from './src/screens'
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -24,30 +11,7 @@ const App = () => {
   return (
     <Provider theme={theme}>
       <NavigationContainer>
-<<<<<<< HEAD
         <DrawerMenu />
-=======
-        <Stack.Navigator
-          initialRouteName="StartScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen name="DetailMenu" component={DetailMenu} />
-          <Stack.Screen name="Cart" component={Cart} />
-          <Stack.Screen name="Profile" component={Profile} />
-          <Stack.Screen name="Checkout" component={Checkout} />
-          <Stack.Screen name="Payment" component={Payment} />
-          <Stack.Screen name="StartScreen" component={StartScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen
-            name="ForgotPasswordScreen"
-            component={ForgotPasswordScreen}
-          />
-        </Stack.Navigator>
->>>>>>> 26acde358bb91f0d7664b41e0486a28b27060cdb
       </NavigationContainer>
     </Provider>
   )
--- src/screens/index.js
+++ src/screens/index.js
@@ -7,11 +7,7 @@ export { default as DetailMenu } from './DetailMenu'
 export { default as Cart } from './Cart'
 export { default as Checkout } from './Checkout'
 export { default as Payment } from './Payment'
-<<<<<<< HEAD
 export { default as DrawerMenu } from './DrawerMenu'
 export { default as StackMenu } from './StackMenu'
 export { default as Tester } from './Tester'
-
-=======
 export { default as Profile } from './Profile'
->>>>>>> 26acde358bb91f0d7664b41e0486a28b27060cdb
