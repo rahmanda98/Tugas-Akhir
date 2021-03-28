d6f0fc8db7890285e66d752bfe6abb726d494353
sepitanekk <septianekokuncahyono@gmail.com>
2021-03-28 07:21:31 +0700

    v2 profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,73 +1,87 @@
 import React from 'react';
-import { ScrollView, TouchableOpacity, StyleSheet, View, Text, Image } from 'react-native';
+import { ScrollView, SafeAreaView, TouchableOpacity, StyleSheet, View, Image } from 'react-native';
+import {Avatar, Title, Caption, Text, TouchableRipple} from 'react-native-paper'
+import Icon from 'react-native-vector-icons/FontAwesome'
 import Logo from '../assets/logo.png'
 const Profile = () => {
     return (
-        <View>
-            <ScrollView>
-                <View style={{padding:10, width:"100%", backgroundColor:'#0000',height:150}}>
-                    <TouchableOpacity>
-                        <Image
-                            source={Logo}
-                            style={{width:30, height:30}}
-                        />
-                        <View>
-
-                        </View>
-                        <View>
-                            
-                        </View>
-                    </TouchableOpacity>
+        <SafeAreaView style={styles.container}>
+            <View style={styles.userInfoSection}>
+                <View style={{flexDirection:'row', marginTop:15}}>
+                    <Avatar.Image source={Logo} size={80} />
+                    <View style={{marginLeft:20}}>
+                        <Title style={[styles.title, {marginTop:15, marginBottom:5}]}>UserName</Title>
+                        <Caption style={styles.caption}>UserName</Caption>
+                    </View>
                 </View>
-                <View style={{alignItems:'center', flexDirection:'row', justifyContent:'center', backgroundColor:'#ffff', width:'90%', padding:20, paddingBottom:'22', borderRadius:10, shadowOpacity:80, elevation:15, marginTop:20 }}>
-                    <Image source={Logo} style={{width:140, height:140, borderRadius:100, marginTop:70 }}/>
-                    <Text style={{fontSize:25, fontWeight:'bold', padding:10}}>Username</Text>
-                    <Text style={{fontSize:15, fontWeight:'bold', padding:10}}>Username</Text>
+            </View>
+            <View style={styles.userInfoSection}>
+                <View style={styles.row}>
+                    <Icon name="map-marker-alt" color="#777777" size={20}/>
+                    <Text style={{color:'#777777'}}>Indonesia</Text>
                 </View>
-                <View style={{alignItems:'center', flexDirection:'row', justifyContent:'center', backgroundColor:'#ffff', width:'90%', padding:20, paddingBottom:'22', borderRadius:10, shadowOpacity:80, elevation:15, marginTop:20, marginBottom:40}}>
-                    <Image style={{width:20, height:20}}/>
-                    <Text style={{fontSize:15, color:'#818181', fontWeight:'bold', marginLeft:10}}>Product Test</Text>
+                <View style={styles.row}>
+                    <Icon name="phone-volume" color="#777777" size={20}/>
+                    <Text style={{color:'#777777'}}>+62909090</Text>
                 </View>
-                <TouchableOpacity  style={{alignItems:'center', flexDirection:'row', justifyContent:'center', backgroundColor:'#ffff', width:'90%', padding:20, paddingBottom:'22', borderRadius:10, shadowOpacity:80, elevation:15, marginTop:20, marginBottom:40, backgroundColor:"#000"}}>
-                    <Text  style={{fontSize:15, color:'#818181', fontWeight:'bold', marginLeft:10}}>Logout</Text>
-                </TouchableOpacity>
-            </ScrollView>
-        </View>
+                <View style={styles.row}>
+                    <Icon name="envelope" color="#777777" size={20}/>
+                    <Text style={{color:'#777777'}}>johnwick@gmail.com</Text>
+                </View>
+            </View>
+        </SafeAreaView>
     )
 }
 
 export default Profile
 
 const styles = StyleSheet.create({
-//   titleWrapper: {
-//     marginTop: 40,
-//   },
-//   title: {
-//     fontSize: 20,
-//     fontWeight: 'bold',
-//     textAlign: 'center',
-//   },
-//   dropdown: {
-//     height: '100%',
-//     width: '100%',
-//     borderRadius: 5,
-//     overflow: 'hidden',
-//     backgroundColor: 'white',
-//     marginTop: 10,
-//     marginBottom: 10,
-//     marginRight: 30,
-//     paddingLeft: 16,
-//   },
-//   buttonCart: {
-//     backgroundColor: '#ffc93c',
-//     height: 60,
-//     borderRadius: 30,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-//   textCart: {
-//     color: 'white',
-//     fontWeight: 'bold',
-//     fontSize: 20,
-//   },
+  container: {
+    flex: 1,
+  },
+  userInfoSection: {
+    paddingHorizontal: 30,
+    marginBottom: 25,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  caption: {
+    fontSize: 14,
+    lineHeight: 14,
+    fontWeight: '500',
+  },
+  row: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  infoBoxWrapper: {
+    borderBottomColor: '#dddddd',
+    borderBottomWidth: 1,
+    borderTopColor: '#dddddd',
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    height: 100,
+  },
+  infoBox: {
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  menuWrapper: {
+    marginTop: 10,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+  },
+  menuItemText: {
+    color: '#777777',
+    marginLeft: 20,
+    fontWeight: '600',
+    fontSize: 16,
+    lineHeight: 26,
+  },
 })
